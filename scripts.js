ee2a41b825a0516c40d3a057e36e5da80f94ee3b
Samuel Alves <49680351+SamuelAlv3s@users.noreply.github.com>
2020-05-17 23:09:32 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -9,3 +9,18 @@ btnMenu.addEventListener("click", () =>{
 btnClose.addEventListener("click", () =>{
     menu.classList.add('hide');
 });
+
+function typeWrite(element){
+    const textArray = element.innerHTML.split('');
+    element.innerHTML = ' ';
+    textArray.forEach( function(letra, index){
+        setTimeout(function(){
+            element.innerHTML += letra;
+        },100 * index);
+    });
+}
+
+const title = document.querySelector('.site-title');
+typeWrite(title);
+
+
